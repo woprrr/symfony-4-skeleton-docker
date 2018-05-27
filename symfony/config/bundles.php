6c2fd735281fb9030a04b8713a18b5197a0abc3c
Alexandre Mallet <woprrr.dev@gmail.com>
2018-05-27 20:01:57 +0200

    Initialize...

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -5,4 +5,5 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
